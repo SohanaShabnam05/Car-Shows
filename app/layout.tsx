798492cbb3f25f4ed69bf6e21e0164269d69058b
sohana <sshabnamzsf@gmail.com>
2024-07-02 16:27:26 +0530

    complete

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import { Navbar, Footer } from "@/components";
+
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+export const metadata: Metadata = {
+	title: "Car Hub",
+	description: "Discovers the best car in the world.",
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang='en'>
+			<body className='relative'>
+				<Navbar />
+				{children}
+				<Footer />
+			</body>
+		</html>
+	);
+}
